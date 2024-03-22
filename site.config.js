229c478419dd15d09bd0da4918ce4aae66e30c87
Defi-Crypto <106485205+DeFi-crypto@users.noreply.github.com>
2024-03-21 21:33:46 -0500

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,13 +1,13 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "FundsFi",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    role: "Founder",
+    bio: "A project that makes privacy friendly and advanced daaps in DeFi. ",
+    email: "harmony.fundsfi@gmail.com",
+    linkedin: "None",
+    github: "None",
     instagram: "",
   },
   projects: [
